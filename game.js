f3b6d245212e55e4d1f06955e227a64928ca2a64
mukeshdani <mukeshdani00@gmail.com>
2021-10-02 23:32:22 +0530

    adding restart the agme

--- game.js
+++ game.js
@@ -6,11 +6,10 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
-//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
 $(document).keypress(function() {
     if (!started) {
   
-      //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+     
       $("#level-title").text("Level " + level);
       nextSequence();
       started = true;
@@ -33,18 +32,18 @@ $(".btn").click(function() {
 });
 
 
-//1. Create a new function called checkAnswer(), it should take one input with the name currentLevel
+
 function checkAnswer(currentLevel) {
 
-    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+   
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
       console.log("success");
 
-      //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+     
       if (userClickedPattern.length === gamePattern.length){
 
-        //5. Call nextSequence() after a 1000 millisecond delay.
+       
         setTimeout(function () {
           nextSequence();
         }, 1000);
@@ -54,17 +53,18 @@ function checkAnswer(currentLevel) {
     } else {
 
       console.log("wrong");
-        //1. In the sounds folder, there is a sound called wrong.mp3, play this sound if the user got one of the answers wrong.
+       
         playSound("wrong");
 
-        //2. In the styles.css file, there is a class called "game-over", apply this class to the body of the website when the user gets one of the answers wrong and then remove it after 200 milliseconds.
+       
         $("body").addClass("game-over");
         setTimeout(function () {
           $("body").removeClass("game-over");
         }, 200);
   
-        //3. Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
+    
         $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
       }
 
     
@@ -91,14 +91,25 @@ function playSound(name) {
   audio.play();
 }
 
-//1. Create a new function called animatePress(), it should take a single input parameter called currentColour.
+
 function animatePress(currentColor) {
 
-    //2. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
+   
     $("#" + currentColor).addClass("pressed");
   
-    //3. use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
+   
     setTimeout(function () {
       $("#" + currentColor).removeClass("pressed");
     }, 100);
   }
+
+
+
+function startOver() {
+
+    
+    level = 0;
+    gamePattern = [];
+    started = false;
+  }
+  
