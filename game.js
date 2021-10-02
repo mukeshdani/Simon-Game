4d502fa1a09e738b7e3cac706a43040ba041527b
mukeshdani <mukeshdani00@gmail.com>
2021-10-02 23:28:06 +0530

    game over

--- game.js
+++ game.js
@@ -54,8 +54,20 @@ function checkAnswer(currentLevel) {
     } else {
 
       console.log("wrong");
+        //1. In the sounds folder, there is a sound called wrong.mp3, play this sound if the user got one of the answers wrong.
+        playSound("wrong");
 
-    }
+        //2. In the styles.css file, there is a class called "game-over", apply this class to the body of the website when the user gets one of the answers wrong and then remove it after 200 milliseconds.
+        $("body").addClass("game-over");
+        setTimeout(function () {
+          $("body").removeClass("game-over");
+        }, 200);
+  
+        //3. Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+      }
+
+    
 
 }
 
