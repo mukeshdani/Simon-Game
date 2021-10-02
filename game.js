04731fac137540c804750d10014712376ca8ab7b
mukeshdani <mukeshdani00@gmail.com>
2021-10-02 23:10:53 +0530

    adding sound on buttonn

--- game.js
+++ game.js
@@ -2,21 +2,17 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
-//3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
 var userClickedPattern = [];
 
-//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+
 $(".btn").click(function() {
 
-  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+  
   var userChosenColour = $(this).attr("id");
 
-  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+ 
   userClickedPattern.push(userChosenColour);
-
-  //console.log(userClickedPattern);
-
+  playSound(userChosenColour);
 });
 
 function nextSequence() {
@@ -27,6 +23,9 @@ function nextSequence() {
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
+  playSound(randomChosenColour);
+}
+function playSound(name) {
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
 }
